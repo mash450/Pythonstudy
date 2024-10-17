727bc54e7eb1818c28d927d1982914b8bae11299
mash450 <abdimash89@gmail.com>
2024-10-17 16:29:16 +0300

    second commit

---
+++ extra_strings_tasks.py
@@ -0,0 +1,54 @@
+# 1
+first_name= "Maslan"
+last_name="Abdi"
+fullname=first_name+" "+last_name
+print(fullname)
+
+# 2
+string="PYTHON"
+firstcharacter= string[0]
+lastcharacter=string[5]
+print(firstcharacter)
+print(lastcharacter)
+
+# 3
+phrase="Learning Python is fun"
+substring=phrase[9:15]
+print(substring)
+
+# 4
+text= "reverse this"
+reversed=text[::-1]
+print(reversed)
+
+# 5
+greeting= "Hello World"
+new_greeting=greeting.replace("World","Python")
+print(new_greeting)
+
+# 6
+msg="Python Programming"
+lowermsg=msg.lower()
+print(lowermsg)
+uppermsg=msg.upper()
+print(uppermsg)
+
+# 7
+sentence= "This is a simple sentence"
+sentence= sentence.count("s")
+print(sentence)
+
+# 8
+quote="The best way to create the future is to create it"
+quote="future" in quote
+print(quote)
+
+# 9
+description="Data Science"
+description=len(description)
+print(description)
+
+# 10
+name=" John Doe "
+name=name.strip()
+print(name)
---
+++ integers_floats.py
@@ -0,0 +1,21 @@
+num1= 200
+num2=500
+# sum divide,multiply,sub  etc
+x=10**5
+print(x)
+
+y=99//2
+print(y)
+
+y=99%2
+print(y)
+
+z=49.1234
+print(round(z,2))
+
+num="1000"
+num=int(num)
+num5="2000"
+num5=int(num5)
+sum=num+num5
+print(sum)
